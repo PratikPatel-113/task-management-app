6cccc8cf6fec8d700f80ec645c548a169103567b
PratikPatel-113 <pratik.patel.workmail@gmail.com>
2025-03-30 16:14:18 +0530

    added routes using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
-import './App.css';
 import Dashboard from './components/DashboardView/Dashboard';
 import TableView from './components/TableView/TableView';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Button } from '@mui/material';
+import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1> Welcome to Task Management App</h1>
-      <TableView />
-      <Dashboard />
-    </div>
+    <Router>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h1> Task Management App</h1>
+
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button color="inherit" href="/">Home</Button>
+          <Button color="inherit" href="/dashboard">Dashboard</Button>
+          <Button color="inherit" href="/table">Table View</Button>
+        </div>
+
+      </div>
+
+      <Routes>
+        <Route path="/" element={<h1 style={{ textAlign: 'center' }}> This is Home Page</h1>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/table" element={<TableView />} />
+      </Routes>
+    </Router >
   );
 }
 
