1146d28aefe5305f8f769649d3f7b8531651421c
PratikPatel-113 <pratik.patel.workmail@gmail.com>
2025-04-01 07:09:31 +0530

    change count for dynamic task generation

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ const tasks = [
 ];
 
 // Generate more tasks dynamically till id 50
-for (let i = 21; i <= 500; i++) {
+for (let i = 21; i <= 50; i++) {
     tasks.push({
         id: i,
         name: `Task ${i}`,
